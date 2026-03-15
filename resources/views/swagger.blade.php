7b86aab611bd4a18db9fdbc38b1b727f7ff3dd48
Matheus Sesso <matheussesso@gmail.com>
2026-03-14 22:25:17 -0300

    Added and configures home route to display Swagger (Interactive documentation).

---
+++ resources/views/swagger.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API BeTalent - Swagger</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            background: #fafafa;
+        }
+
+        #swagger-ui {
+            min-height: 100vh;
+        }
+    </style>
+</head>
+<body>
+<div id="swagger-ui"></div>
+
+<script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+<script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+<script>
+    window.onload = () => {
+        window.ui = SwaggerUIBundle({
+            url: '/api-betalent.json',
+            dom_id: '#swagger-ui',
+            deepLinking: true,
+            presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset,
+            ],
+            layout: 'StandaloneLayout',
+            persistAuthorization: true,
+        });
+    };
+</script>
+</body>
+</html>
